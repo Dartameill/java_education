8a1daa890287a1976670293ca3152ae385ffdcfb
Sergey Padaru <siarhei.padaru@appcast.io>
2021-11-02 19:34:49 +0300

    Initial commit

---
+++ src/com/company/Solution.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+
+
+        while(true) {
+
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+
+        if (name.equals("exit")) {
+            System.out.println("exit from system is start");
+            break;
+        } else if (name != "exit") {
+            System.out.println("you write " + name);
+
+        }
+            }
+
+        System.out.println("exit from system is complete");
+
+
+}
+}
+
+
