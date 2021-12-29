286f1a58b2cb688c2202b43216753cf522b12192
Dartameill <serggarelov@gmail.com>
2021-12-29 18:35:21 +0300

    New Lesson 8

---
+++ src/com/company/lesson8/EnterWord/EnterUserWord.java
@@ -0,0 +1,8 @@
+package com.company.lesson8.EnterWord;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public interface EnterUserWord {
+    ArrayList<String> EnterWordTo(ArrayList<String> arrayList) throws IOException;
+}
