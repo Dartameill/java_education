05a3bd035789df7f0c28da252f8ac0414eb09d0c
Dartameill <serggarelov@gmail.com>
2021-12-29 18:02:43 +0300

    Revert "Part of Lesson 8"
    This reverts commit d81259b10cd3c5c1cc71e18f5122de463fc6e9b4.

--- src/com/company/lesson8/InpOut/InputAndOutput.java
+++
@@ -1,9 +0,0 @@
-package com.company.lesson8.InpOut;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public interface InputAndOutput {
-     ArrayList<String> OutAndInt(ArrayList<String> arrayList);
-     ArrayList<String> isEmptyArray(ArrayList<String> list);
-}
