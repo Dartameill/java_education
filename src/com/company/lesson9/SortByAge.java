b08963c53c113dc75b105d52fb0f8b10f547523f
Dartameill <serggarelov@gmail.com>
2022-01-10 13:35:03 +0300

    Lesson 9

---
+++ src/com/company/lesson9/SortByAge.java
@@ -0,0 +1,9 @@
+package com.company.lesson9;
+
+
+import java.util.Collection;
+import java.util.List;
+
+public interface SortByAge {
+    Collection<User> ByAge(int age, Collection<User> collection);
+}
